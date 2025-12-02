97c6ab8140df22ee75a5344a1f78b39727200e77
Andrew Domanski <andqok@gmail.com>
2025-12-02 09:52:49 +0200

    QCD-3434 move function outside unnecessary scope to improve readability

--- src/QencodeWebRTC.js
+++ src/QencodeWebRTC.js
@@ -175,6 +175,50 @@ function delayedCall(fn, args, delay) {
   });
 }
 
+// From https://webrtchacks.com/limit-webrtc-bandwidth-sdp/
+function setBitrateLimit(sdp, media, bitrate) {
+  
+  let lines = splitSdpLines(sdp);
+  let line = -1;
+  
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].indexOf('m=' + media) === 0) {
+      line = i;
+      break;
+    }
+  }
+  if (line === -1) {
+    // Could not find the m line for media
+    return sdp;
+  }
+  
+  // Pass the m line
+  line++;
+  
+  // Skip i and c lines
+  while (lines[line].indexOf('i=') === 0 || lines[line].indexOf('c=') === 0) {
+    
+    line++;
+  }
+  
+  // If we're on a b line, replace it
+  if (lines[line].indexOf('b') === 0) {
+    
+    lines[line] = 'b=AS:' + bitrate;
+    
+    return joinSdpLines(lines);
+  }
+  
+  // Add a new b line
+  let newLines = lines.slice(0, line)
+  
+  newLines.push('b=AS:' + bitrate)
+  newLines = newLines.concat(lines.slice(line, lines.length))
+  
+  return joinSdpLines(newLines)
+}
+
+
 function addMethod(instance) {
 
     function errorHandler(error) {
@@ -277,48 +321,6 @@ function addMethod(instance) {
             });
     }
 
-    // From https://webrtchacks.com/limit-webrtc-bandwidth-sdp/
-    function setBitrateLimit(sdp, media, bitrate) {
-
-        let lines = splitSdpLines(sdp);
-        let line = -1;
-
-        for (let i = 0; i < lines.length; i++) {
-            if (lines[i].indexOf('m=' + media) === 0) {
-                line = i;
-                break;
-            }
-        }
-        if (line === -1) {
-            // Could not find the m line for media
-            return sdp;
-        }
-
-        // Pass the m line
-        line++;
-
-        // Skip i and c lines
-        while (lines[line].indexOf('i=') === 0 || lines[line].indexOf('c=') === 0) {
-
-            line++;
-        }
-
-        // If we're on a b line, replace it
-        if (lines[line].indexOf('b') === 0) {
-
-            lines[line] = 'b=AS:' + bitrate;
-
-            return joinSdpLines(lines);
-        }
-
-        // Add a new b line
-        let newLines = lines.slice(0, line)
-
-        newLines.push('b=AS:' + bitrate)
-        newLines = newLines.concat(lines.slice(line, lines.length))
-
-        return joinSdpLines(newLines)
-    }
 
     function initWebSocket(connectionUrl) {
 
