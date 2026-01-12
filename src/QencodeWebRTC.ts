43069c5d86c52866390771d19fcc6746632af270
Andrew Domanski <andqok@gmail.com>
2026-01-12 14:08:28 +0200

    QCD-3501 remove tmp code

--- src/QencodeWebRTC.ts
+++ src/QencodeWebRTC.ts
@@ -1202,7 +1202,6 @@ QencodeWebRTC.create = function (config: CreateConfig = {}) {
   console.info('QencodeWebRTC ', '2025-12-22');
   const instance = initConfig(config);
   addMethod(instance);
-  document.body.classList.add('my-custom-class-3')
   return instance;
 };
 
