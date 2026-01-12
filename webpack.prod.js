6eb08a49a0626880074bb70f32406ccd1d2d2ebf
Andrew Domanski <andqok@gmail.com>
2026-01-12 13:29:44 +0200

    QCD-3501 try using css folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 
 // Ensure output directory exists
-const outputPath = path.resolve(__dirname, 'demo', 'js');
+const outputPath = path.resolve(__dirname, 'demo', 'css');
 if (!fs.existsSync(outputPath)) {
   fs.mkdirSync(outputPath, { recursive: true });
 }
